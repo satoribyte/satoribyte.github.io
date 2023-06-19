60dd108adbe55c7c6e162df1b86c2cc72aba9df4
Deni Gentar Candana <136276001+satoribyte@users.noreply.github.com>
2023-06-20 00:42:21 +0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,46 +1,43 @@
-// Fungsi untuk menghasilkan sitemap.xml
-function generateSitemap(directory) {
-  var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
-  xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
+    $(document).ready(function() {
+      generateSitemap();
+    });
 
-  // AJAX request untuk mendapatkan daftar file dan folder di direktori
-  var request = new XMLHttpRequest();
-  request.open('GET', 'get_files.php?dir=' + encodeURIComponent(directory), true);
-  request.onreadystatechange = function() {
-    if (request.readyState === 4 && request.status === 200) {
-      var files = JSON.parse(request.responseText);
+    function generateSitemap() {
+      var directory = '/';
 
-      files.forEach(function(file) {
-        var path = directory + '/' + file.name;
-        var url = 'http://localhost' + path; // Ubah sesuai dengan URL localhost Anda
+      $.ajax({
+        url: 'startsitemap.php',
+        method: 'GET',
+        data: { dir: directory },
+        dataType: 'json',
+        success: function(files) {
+          var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
+          xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
-        if (file.type === 'directory') {
-          url += '/';
-          xml += '  <url>\n';
-          xml += '    <loc>' + url + '</loc>\n';
-          xml += '  </url>\n';
+          files.forEach(function(file) {
+            var path = directory + '/' + file.name;
+            var url = 'http://localhost' + path;
 
-          xml += generateSitemap(path); // Rekursif untuk menjelajahi folder
-        } else {
-          xml += '  <url>\n';
-          xml += '    <loc>' + url + '</loc>\n';
-          xml += '  </url>\n';
+            if (file.type === 'directory') {
+              url += '/';
+            }
+
+            xml += '  <url>\n';
+            xml += '    <loc>' + url + '</loc>\n';
+            xml += '  </url>\n';
+          });
+
+          xml += '</urlset>';
+
+          // Simpan sitemap.xml ke file
+          var downloadLink = document.createElement('a');
+          downloadLink.href = 'data:text/xml;charset=utf-8,' + encodeURIComponent(xml);
+          downloadLink.download = 'sitemap.xml';
+          downloadLink.innerText = 'Download Sitemap';
+          document.body.appendChild(downloadLink);
+        },
+        error: function() {
+          console.log('Error generating sitemap.');
         }
       });
-
-      xml += '</urlset>';
-
-      // Simpan sitemap.xml ke file
-      var downloadLink = document.createElement('a');
-      downloadLink.href = 'data:text/xml;charset=utf-8,' + encodeURIComponent(xml);
-      downloadLink.download = 'sitemap.xml';
-      downloadLink.innerText = 'Download Sitemap';
-      document.body.appendChild(downloadLink);
-    }
-  };
-  request.send();
-}
-
-var directory = '/r'; // Ubah dengan path ke folder root file manager web Anda
-generateSitemap(directory);
-
+              }
