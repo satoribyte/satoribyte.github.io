32b7fb963308cb6740038a3f5407cd698eb6c711
Deni Gentar Candana <136276001+satoribyte@users.noreply.github.com>
2023-06-20 00:34:24 +0700

    Update sitemap.js

--- sitemap.js
+++ sitemap.js
@@ -1,46 +1,46 @@
-function generateSitemap() {
-  var directory = '/'; 
-  var sitemapUrls = [];
-
-  function traverseDirectory(directoryPath) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', directoryPath, true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var fileList = xhr.responseText.split('\n');
-        for (var i = 0; i < fileList.length; i++) {
-          var item = fileList[i].trim();
-          if (item !== '') {
-            var url = directoryPath + '/' + item;
-            if (item.indexOf('.') === -1) { // Jika item adalah folder
-              traverseDirectory(url);
-            } else { // Jika item adalah file
-              sitemapUrls.push(url);
-            }
-          }
-        }
-      }
-    };
-    xhr.send();
-  }
-
-  traverseDirectory(directory);
-
+// Fungsi untuk menghasilkan sitemap.xml
+function generateSitemap(directory) {
   var xml = '<?xml version="1.0" encoding="UTF-8"?>\n';
   xml += '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n';
 
-  for (var j = 0; j < sitemapUrls.length; j++) {
-    xml += '  <url>\n';
-    xml += '    <loc>' + sitemapUrls[j] + '</loc>\n';
-    xml += '  </url>\n';
-  }
+  // AJAX request untuk mendapatkan daftar file dan folder di direktori
+  var request = new XMLHttpRequest();
+  request.open('GET', 'get_files.php?dir=' + encodeURIComponent(directory), true);
+  request.onreadystatechange = function() {
+    if (request.readyState === 4 && request.status === 200) {
+      var files = JSON.parse(request.responseText);
 
-  xml += '</urlset>';
+      files.forEach(function(file) {
+        var path = directory + '/' + file.name;
+        var url = 'http://localhost' + path; // Ubah sesuai dengan URL localhost Anda
 
-  var downloadLink = document.createElement('a');
-  downloadLink.href = 'data:text/xml;charset=utf-8,' + encodeURIComponent(xml);
-  downloadLink.download = 'sitemap.xml';
-  downloadLink.innerText = 'Download Sitemap';
+        if (file.type === 'directory') {
+          url += '/';
+          xml += '  <url>\n';
+          xml += '    <loc>' + url + '</loc>\n';
+          xml += '  </url>\n';
 
-  document.body.appendChild(downloadLink);
-          }
+          xml += generateSitemap(path); // Rekursif untuk menjelajahi folder
+        } else {
+          xml += '  <url>\n';
+          xml += '    <loc>' + url + '</loc>\n';
+          xml += '  </url>\n';
+        }
+      });
+
+      xml += '</urlset>';
+
+      // Simpan sitemap.xml ke file
+      var downloadLink = document.createElement('a');
+      downloadLink.href = 'data:text/xml;charset=utf-8,' + encodeURIComponent(xml);
+      downloadLink.download = 'sitemap.xml';
+      downloadLink.innerText = 'Download Sitemap';
+      document.body.appendChild(downloadLink);
+    }
+  };
+  request.send();
+}
+
+var directory = '/r'; // Ubah dengan path ke folder root file manager web Anda
+generateSitemap(directory);
+
