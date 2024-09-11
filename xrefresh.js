c802091d5c7eec2819300b42258a22eb84e0aa28
Satoribyte <136276001+satoribyte@users.noreply.github.com>
2024-09-11 18:26:51 +0700

    Create xrefresh.js

---
+++ xrefresh.js
@@ -0,0 +1,37 @@
+(function() {
+    var sP = false;
+
+    try {
+        var o = Object.defineProperty({}, 'passive', {
+            get: function() { sP = true; }
+        });
+        window.addEventListener("test", null, o);
+    } catch (e) {}
+
+    var yS = 0, pD = false;
+
+    function pTR(e) {
+        if (pD && e.cancelable) {
+            e.preventDefault();
+        }
+    }
+
+    document.addEventListener('touchstart', function(e) {
+        if (e.touches.length === 1) {
+            yS = e.touches[0].clientY;
+            pD = window.pageYOffset === 0;
+        }
+    }, sP ? { passive: true } : false);
+
+    document.addEventListener('touchmove', function(e) {
+        var cY = e.touches[0].clientY;
+        if (pD && cY > yS) {
+            pTR(e);
+        }
+    }, sP ? { passive: false } : false);
+
+    document.addEventListener('touchend', function() {
+        pD = false;
+    });
+})();
+  
